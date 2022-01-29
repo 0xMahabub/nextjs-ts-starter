141209a8be93780806740f4b157e36076dc0431d
Mahabub <mahabub212002072@gmail.com>
2022-01-29 14:31:25 +0600

    staged-v1.0

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ['./components/**/*.{js,jsx,ts,tsx}', './pages/**/*.{js,jsx,ts,tsx}'],
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  variants: {
+    extend: {},
+  },
+}
